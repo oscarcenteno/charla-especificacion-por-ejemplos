102480b7c5912028d7b38fed5b92cda511cfbb0a
oscarcenteno <oscar.centeno@outlook.com>
2015-06-02 16:26:49 -0600

    Ajusto para que sea Especificaciones.

---
+++ Demo/Especificaciones/ConversionesDeParametros.vb
@@ -0,0 +1,30 @@
+﻿Imports TechTalk.SpecFlow
+Imports TechTalk.SpecFlow.Assist
+Imports LogicaDeNegocio.EntregasGratuitas
+Imports LogicaDeNegocio.Garantias
+
+<Binding()> _
+Public Class ConversionesDeParametros
+
+    <StepArgumentTransformation()>
+    Public Function TransformeAPorcentaje(elPorcentaje As String) As Porcentaje
+        Return Porcentaje.Parse(elPorcentaje)
+    End Function
+
+    <StepArgumentTransformation()>
+    Public Function TransformeAReal(elMonto As String) As MontoRealExcel
+        Return MontoRealExcel.Parse(elMonto)
+    End Function
+
+    <StepArgumentTransformation()>
+    Public Function TransformeAEntero(elMonto As String) As MontoEnteroExcel
+        Return MontoEnteroExcel.Parse(elMonto)
+    End Function
+
+    <StepArgumentTransformation()>
+    Public Function TransformeAMontoEnDolares(monto As String) As MontoEnDolares
+        Return MontoEnDolares.Parse(monto)
+    End Function
+
+End Class
+
---
+++ Demo/Especificaciones/Garantías de valores/Formulas financieras/ValorDeMercadoSteps.vb
@@ -0,0 +1,39 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports TechTalk.SpecFlow
+Imports LogicaDeNegocio
+Imports LogicaDeNegocio.Garantias
+
+Namespace Escenarios
+
+    ' For additional details on SpecFlow step definitions see http://go.specflow.org/doc-stepdef
+
+    <Binding()> _
+    Public NotInheritable Class ValorDeMercadoSteps
+#Region "valor de mercado"
+
+        Private _montoNominal As MontoRealExcel
+        Private _valorDeMercadoObtenido As Decimal
+
+        <TechTalk.SpecFlow.Given("que la emisión reporta un ""(.*)""")> _
+        Public Sub DadoQueLaEmisionReportaUn(ByVal montoNominal As MontoRealExcel)
+            _montoNominal = montoNominal
+        End Sub
+
+        <TechTalk.SpecFlow.When("se valora con el ""(.*)"" reportado en el vector de precios para la emisión")> _
+        Public Sub CuandoSeValoraConElReportadoEnElVectorDePreciosParaLaEmision(ByVal precioDeMercado As MontoRealExcel)
+            _valorDeMercadoObtenido = FormulasDeValoracion.CalcularValorDeMercado(_montoNominal.Valor, precioDeMercado.Valor)
+        End Sub
+
+        <TechTalk.SpecFlow.Then("se obtiene el ""(.*)"" de la emisión")> _
+        Public Sub EntoncesSeObtieneElDeLaEmision(ByVal valorDeMercadoEsperado As MontoRealExcel)
+            Assert.AreEqual(Of Decimal)(valorDeMercadoEsperado.Valor, _valorDeMercadoObtenido)
+        End Sub
+
+#End Region
+
+    End Class
+
+End Namespace
